f4e7b2cfe29d884419842f34931b1521c39d2b8b
Adrian <adrianevangelista@gmail.com>
2023-11-24 17:43:01 -0300

    Microservices dockernization

--- account/src/main/java/com/backend/test/account/config/RabbitMQConfig.java
+++ account/src/main/java/com/backend/test/account/config/RabbitMQConfig.java
@@ -11,6 +11,27 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+    @Value("${rabbitmq.queue.name}")
+    String queueName;
+    @Value("${rabbitmq.exchange.name}")
+    String exchange;
+    @Value("${rabbitmq.routingKey.name}")
+    private String routingKey;
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(exchange);
+    }
+
+    @Bean
+    Binding binding() {
+        return BindingBuilder.bind(queue()).to(exchange()).with(routingKey);
+    }
 
     @Bean
     public MessageConverter jsonMessageConverter() {
